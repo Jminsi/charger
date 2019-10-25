6ff08f3b6be9150259ba10207016e6628456a521
Minsi <k8jami00@students.oamk.fi>
2019-10-25 19:59:06 +0300

    updated app with css

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Auth from './components/Auth';
 import RegisterView from './components/RegisterView';
 import ChargerList from './components/ChargerList'
@@ -19,17 +19,16 @@ class App extends React.Component {
       chargers: [],
       history: [],
       searchFilter: "",
-      listMode: "grid",
-      userFullName: null,
-      chargingSecs: 0
-      
+      userFullName: 'null',
+      chargingOngoing: false,
+      chargingSecs: 0      
     }
   }
 
 
   componentDidMount = () =>
   {    
-    console.log('componentDidMount history=' + this.state.history);
+    //console.log('componentDidMount history=' + this.state.history);
     axios.get(constants.baseAddress + '/chargers').then(result => {
       this.setState({ chargers: result.data.chargers });
       console.log('componentDidMount() chargers:' + result.data.chargers);      
@@ -38,7 +37,8 @@ class App extends React.Component {
       console.error(error);
     })
 
-    //TODO remove after work done
+    //TODO remove when this works
+    /*
     axios.get(constants.baseAddress + '/users/historyy').then(result => {
       this.setState({ history: result.data.history });
       console.log('componentDidMount() history:' + result.data.history);      
@@ -46,11 +46,12 @@ class App extends React.Component {
     .catch(error => {
       console.error(error);
     })
+    */
   }
 
 
   registerUser = (full_name, username, password) => {
-    console.log('reg: ' + full_name + ' ' + username + '  ' + password);
+    console.log('registerUser(): ' + full_name + ' ' + username + '  ' + password);
     axios.post(constants.baseAddress + '/users/register', {
       full_name: full_name,
       username:  username,
@@ -65,35 +66,48 @@ class App extends React.Component {
   }
 
 
-/*  getUserHistory = () => {
+  /* Loads users charging history, user needs to be login/authenticated at first */
+  getUserHistory = () => {
     console.log('getUserHistory()')
     axios.get(constants.baseAddress + '/users/history', Auth.getAxiosAuth()).then(results => {
       this.setState({ history: results.data.history });
-      //console.log(results.data.history);
-      //return results.data.history;
     })
-    console.log('getUserHistory(): ' + this.state.history)
+  }
+
+
+/*
+  getUserHistory = () => {
+    axios.get(constants.baseAddress + '/users/historyy').then(result => {
+      this.setState({ history: result.data.history });
+      console.log('componentDidMount() history:' + result.data.history);      
+    })
+    .catch(error => {
+      console.error(error);
+    })
   }
 */
 
-
+  /* Called during user login, saves users full name */
   onLogin = (userFullNameFromApi) => {
     console.log('onLogin() userFullNameFromApi=' + userFullNameFromApi);
     this.setState({ userFullName: userFullNameFromApi });
   }
 
 
+  /* Called durign logout, clears users full name */
   onLogout = () => {
     console.log("onLogout()");
     this.setState({ userFullName: null });
   }
 
 
+  /* Updates charger search filter */
   searchFilterUpdate = (newValue) => {
     this.setState({ searchFilter: newValue });
   }
 
 
+  /* Returns charger info for given charger ID */
   getChargerInfo = (chargerId) => {
     return this.state.chargers.find(charger => charger.id === chargerId);
   }
@@ -106,14 +120,42 @@ class App extends React.Component {
     this.chargerTimer = setInterval(function(){
       _this.setState({chargingSecs: _this.state.chargingSecs + 1})
     }, 1000)
+    this.setState({chargingOngoing: true})
   }
+
 
   /* Stops interval timer */
   stopTimer = () => {
     clearInterval(this.chargerTimer);
+    this.setState({chargingOngoing: false})
   }
 
 
+  /* Sets charging secs to zero */
+  resetChargingSecs = () => {
+    this.setState({ chargingSecs: 0 });
+  }
+
+
+  /* Saves charge information  to users history */
+  saveChargeToHistory = (chargerId, timeStamp, chargingTimeSecs, chargedEnergyKwh, costCents) => {
+    console.log('saveChargeToHistory(): id=' + chargerId + ' ts=' + timeStamp + '  sec=' + chargingTimeSecs + ' kwh=' + chargedEnergyKwh + ' cost=' + costCents );
+
+//     axios.get(constants.baseAddress + '/users/history', Auth.getAxiosAuth()).then(results => {
+    axios.post(constants.baseAddress + '/users/savehistory', {
+      charger_id: chargerId,
+      time: timeStamp,
+      charging_time_secs: chargingTimeSecs,
+      charged_energy_kwh: chargedEnergyKwh,
+      cost_cents: costCents
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 
 
   render()
@@ -129,12 +171,22 @@ class App extends React.Component {
               searchFilter={ this.state.searchFilter }
               onSearchFilterUpdate={ this.searchFilterUpdate }
               userLogout={ this.onLogout }
+              getUserHistory={ this.getUserHistory }
+              {...routeProps}
               />
         } />
 
         <Route path="/register"    exact render={ routeProps => <RegisterView registerUser={ this.registerUser } {...routeProps} /> }/>
         <Route path="/login"       exact render={ routeProps => <LoginView loginSuccess={ this.onLogin } storeUserInfo={ this.storeUserInfo } {...routeProps} /> }/>
-        <Route path="/charger/:id" exact render={ routeProps => <ChargerView {...routeProps} chargingSecs={this.state.chargingSecs} getChargerInfo={ this.getChargerInfo } startTimer={this.startTimer}  stopTimer={this.stopTimer}   /> } />
+        <Route path="/charger/:id" exact render={ routeProps => <ChargerView {...routeProps} 
+          chargingOngoing={this.state.chargingOngoing} 
+          chargingSecs={this.state.chargingSecs} 
+          getChargerInfo={ this.getChargerInfo } 
+          startTimer={this.startTimer}  
+          stopTimer={this.stopTimer} 
+          resetChargingSecs={this.resetChargingSecs} 
+          saveChargeToHistory={this.saveChargeToHistory}  /> } 
+        />
         <Route path="/history"     exact render={ (routeProps) => <HistoryList  history={ this.state.history }  getChargerInfo={ this.getChargerInfo }       /> }/>
       </Router>
     )
