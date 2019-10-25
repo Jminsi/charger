d095113825577b1c17896d493bc78d4ee4c38ed3
Minsi <k8jami00@students.oamk.fi>
2019-10-25 23:56:22 +0300

    added db init commands but they are commented out

--- api/index.js
+++ api/index.js
@@ -143,15 +143,40 @@ app.post('/users/savehistory',
 /* DB init */
 Promise.all(
   [
-/*
-      db.query(`CREATE TABLE IF NOT EXISTS users(
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          username VARCHAR(32),
-          password VARCHAR(256)
-  
-      ))
-      */
       // Add more table create statements if you need more tables
+/*
+        db.query(`CREATE TABLE IF NOT EXISTS users (
+          id int(11) NOT NULL AUTO_INCREMENT,
+          username varchar(45) DEFAULT NULL,
+          password varchar(128) DEFAULT NULL,
+          full_name varchar(45) DEFAULT NULL,
+          PRIMARY KEY (id))`)
+
+        db.query(`CREATE TABLE IF NOT EXISTS history (
+          id int(11) NOT NULL AUTO_INCREMENT,
+          user_id int(11) NOT NULL,
+          charger_id int(11) NOT NULL,
+          time varchar(45) DEFAULT NULL,
+          charging_time_secs int(11) DEFAULT NULL,
+          charged_energy_kwh int(11) DEFAULT NULL,
+          cost_cents int(11) DEFAULT NULL,
+          PRIMARY KEY (id))`)
+
+        db.query(`CREATE TABLE IF NOT EXISTS chargers (
+          id int(11) NOT NULL AUTO_INCREMENT,
+          digit varchar(45) DEFAULT NULL,
+          city varchar(45) DEFAULT NULL,
+          address varchar(45) DEFAULT NULL,
+          name varchar(45) DEFAULT NULL,
+          type enum('Slow','Fast') DEFAULT NULL,
+          power_kw int(11) DEFAULT NULL,
+          pricing_mode enum('free','min','kwh') DEFAULT NULL,
+          price_cents int(11) DEFAULT NULL,
+          status enum('Free','Taken') DEFAULT NULL,
+          PRIMARY KEY (id))`)
+          
+        db.query(`INSERT INTO chargers VALUES (1,'AAA1','Oulu','Kasarmintie','Oulun Energian toimitalon piha','Slow',25,'free',0,'Free'),(2,'AAA2','Oulu','Metsokankaantie 5','K-Citymarket Oulu Kaakkuri','Fast',50,'kwh',11,'Taken'),(3,'AAA3','Ii','Piisilta 1','Micropolis, parkkipaikka','Slow',35,'min',99,'Free'),(4,'AAA4','Helsinki','Elielinaukio','P-Eliel','Slow',35,'min',8,'Free'),(5,'AAA5','Kemi','Tietokatu 1','Kosmos-rakennus','Slow',35,'min',18,'Free'),(6,'AAA6','Kuopio','Kauppakatu 45','Toriparkki','Slow',35,'kwh',8,'Free'),(7,'AAA7','Kurikka','Keskuspuistikko','Kurikan Lukio','Slow',35,'free',0,'Free'),(8,'ASC3','Eurajoki','Kirkkotie 3','Osuuspankin piha-alue','Slow',22,'min',20,'Free'),(9,'GE3F','Imatra','Tiedonkatu 2','ABC Imatra','Fast',135,'kwh',28,'Free'),(10,'BD3X','Joensuu','Torikatu 16','Hotel Greenstar parkkihalli','Slow',22,'free',0,'Free'),(11,'BDZ1','Kokkola','Talonpojankatu 2','KEBA KeContact P20','Fast',120,'kwh',80,'Taken'),(12,'JEC5','Lahti','Kiitokatu 2','ABC Kivimaa','Slow',22,'min',10,'Free'),(13,'KHV6','Oulu','Kiviharjuntie 5','Technopolis Sairaalaparkki Parkkihalli','Fast',60,'kwh',18,'Free'),(14,'IXS3','Oulu','Nuottasaarentie 1','ABC Prisma Limingantulli','Slow',22,'min',15,'Free'),(15,'V6HB','Raisio','Nesteentie 36','Hiabin toimipiste','Fast',75,'free',0,'Free'),(16,'BS4X','Rovaniemi','Koskikatu 25','Kauppakeskus Rinteenkulma','Slow',22,'min',20,'Taken'),(17,'PO8V','Turku','Linnankatu 65','Turku Energian toimitalo','Fast',50,'kwh',25,'Free'),(18,'UIC3','Vantaa','Kiitoradantie 2','Volkswagen Center Airport','Slow',22,'min',8,'Free'),(19,'QWX3','Ylivieska','Vierimaantie 7','Centrian parkkipaikka','Fast',80,'kwh',20,'Taken'),(20,'MND4','Nokia','Kerhokatu 10','Nokian voimalaitoksen latausasema','Slow',22,'min',15,'Free'),(21,'NSS2','Muonio','Harrinivantie 28','Harrinivan lomakeskus','Slow',22,'free',0,'Free');`)
+*/
   ]
 ).then(() => {
   console.log('database initialized');
